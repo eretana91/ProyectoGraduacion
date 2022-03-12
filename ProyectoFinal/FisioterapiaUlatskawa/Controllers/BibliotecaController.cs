6f781e0b346619cc570e36399971d01e835295a5
OSCAR BARRANTES <oscarbarrantes1@gmail.com>
2022-03-11 20:22:18 -0600

    Biblioteca
    Create Index

--- ProyectoFinal/FisioterapiaUlatskawa/Controllers/BibliotecaController.cs
+++ ProyectoFinal/FisioterapiaUlatskawa/Controllers/BibliotecaController.cs
@@ -1,4 +1,5 @@
-﻿using FisioterapiaUlatskawa.Models;
+﻿using FisioterapiaUlatskawa.DataModel;
+using FisioterapiaUlatskawa.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,15 @@ namespace FisioterapiaUlatskawa.Controllers
 {
     public class BibliotecaController : Controller
     {
+
+
+        private readonly Context context;
+
+        public BibliotecaController()
+        {
+            context = new Context();
+        }
+
         // GET: Biblioteca
         public ActionResult Index()
         {
@@ -42,12 +52,115 @@ namespace FisioterapiaUlatskawa.Controllers
                 titulo = "FSADFSAD"
             });
 
-
-
-
-
-
             return View(listado);
         }
+
+
+
+        #region GET: Inventario/Create
+        public ActionResult Create()
+        {
+            BibliotecaViewModel bibliotecaViewModel = new BibliotecaViewModel();
+            try
+            {
+
+
+                List<SelectListItem> ListaTipoInventario = new List<SelectListItem>();
+
+                var result = context.ListarTipoProducto().ToList();
+
+                foreach (var dato in result)
+                {
+                    ListaTipoInventario.Add(new SelectListItem
+                    {
+                        Value = dato.TipoProducto.ToString(),
+                        Text = dato.nombreTipoProducto
+                    });
+                };
+
+                //bibliotecaViewModel.ListaTipoProducto = ListaTipoInventario;
+
+                return View(bibliotecaViewModel);
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    TempData["ErrorMessage"] = ex.InnerException.Message;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = ex.Message;
+                }
+            }
+
+            return RedirectToAction("Index", "Inventario");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "tituloVideo,urlVideo,descripcionVideo")] BibliotecaViewModel bibliotecaVM)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    //var resulta = context.(
+
+                    //  bibliotecaVM.titulo,
+                    //  bibliotecaVM.url,
+                    //  bibliotecaVM.descripcion
+                    //   ).FirstOrDefault();
+
+
+                    TempData["Message"] = "Video Insertado correctamente";
+
+                    return RedirectToAction("Index", "Biblioteca");
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException != null)
+                    {
+                        TempData["ErrorMessage"] = ex.InnerException.Message;
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = ex.Message;
+                    }
+                }
+            }
+            else
+            {
+
+
+                var error = ModelState.Select(x => x.Value.Errors)
+                           .Where(y => y.Count > 0)
+                           .FirstOrDefault();
+
+                TempData["ErrorMessage"] = error[0].ErrorMessage;
+
+
+            }
+
+            List<SelectListItem> ListaTipoProducto = new List<SelectListItem>();
+
+            var result = context.ListarTipoProducto().ToList();
+
+            foreach (var dato in result)
+            {
+                ListaTipoProducto.Add(new SelectListItem
+                {
+                    Value = dato.TipoProducto.ToString(),
+                    Text = dato.nombreTipoProducto
+                });
+            };
+
+            //bibliotecaVM.Lista = ListaTipoProducto;
+
+            return View(bibliotecaVM);
+        }
+        #endregion
     }
 }
