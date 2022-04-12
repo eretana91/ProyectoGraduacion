1fbdb4cc173eb87adbfad51b693850123c7625e8
eretana91 <eretana91@gmail.com>
2022-04-12 07:05:08 -0600

    restore Proyect

--- ProyectoFinal/FisioterapiaUlatskawa/Controllers/CitasController.cs
+++ ProyectoFinal/FisioterapiaUlatskawa/Controllers/CitasController.cs
@@ -1,8 +1,5 @@
 ﻿using FisioterapiaUlatskawa.DataModel;
-<<<<<<< Updated upstream
-=======
 using FisioterapiaUlatskawa.Tags;
->>>>>>> Stashed changes
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +8,8 @@ using System.Web.Mvc;
 
 namespace FisioterapiaUlatskawa.Controllers
 {
-<<<<<<< Updated upstream
-=======
     [Authorize]
     [SessionExpireFilter]
->>>>>>> Stashed changes
     public class CitasController : Controller
     {
         private Context db = new Context();
@@ -37,17 +31,10 @@ namespace FisioterapiaUlatskawa.Controllers
             foreach (var item in data)
             {
                 var inicio = (DateTime)item.StartDate;
-<<<<<<< Updated upstream
-                var start = inicio.ToString("yyyy-MM-dd hh:mm:ss");
-
-                var final = (DateTime)item.EndDate;
-                var end = final.ToString("yyyy-MM-dd hh:mm:ss");
-=======
                 var start = inicio.ToString("yyyy-MM-dd HH:mm");
 
                 var final = (DateTime)item.EndDate;
                 var end = final.ToString("yyyy-MM-dd HH:mm");
->>>>>>> Stashed changes
 
                 eventos.Add(new Models.WebAPIRecurringEvent
                 {
